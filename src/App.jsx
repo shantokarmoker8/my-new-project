591ac06efbd1a4ff41193da3e0cbed0ba491d53c
shantokarmoker8 <shantokarmoker8@gmail.com>
2025-06-11 22:26:37 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <Navbar />
     </>
   );
 }
